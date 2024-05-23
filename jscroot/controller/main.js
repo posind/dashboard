64beb06bc1777b1abb026bc878d570cc6067bb99
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-23 21:16:14 +0700

    coba dulu header

--- jscroot/controller/main.js
+++ jscroot/controller/main.js
@@ -1,15 +1,27 @@
 //lib call
-import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.4/croot.js";
+import { insertHTML,getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
+import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
+import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
+import { setValue } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 //internal call
 import { url,id } from "../url/config.js";
 import { getContentURL,getURLContentJS } from "../url/content.js";
 
+
+
+const urlGetDataUser="https://api.do.my.id/data/user";
 
 export function runAfterHashChange(evt){
     insertHTML(getContentURL(),id.content,runAfterContent);
 }
 
 export function runAfterHeader(){
+    //set header and cookies
+    if (getCookie("login")===""){
+        redirect("/signin");
+    }else{
+        getJSON(urlGetDataUser,"login",getCookie("login"),getUserFunction);
+    }
     insertHTML(url.template.navbar,id.navbar,runAfterNavbar);
 }
 
@@ -24,3 +36,11 @@ export async function runAfterContent(){
     module.main();
     console.log(urljs);
 }
+
+function getUserFunction(result){
+    if (result.status!==404){
+        setValue("headerlogoname",result.data.name);
+    }else{
+        redirect("/signup");
+    }
+  }
