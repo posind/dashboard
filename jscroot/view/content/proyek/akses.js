3f106051e7e63007afe25124792ca2cd7ffe642c
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-25 08:17:56 +0700

    oke bisa masup

--- jscroot/view/content/proyek/akses.js
+++ jscroot/view/content/proyek/akses.js
@@ -1,4 +1,5 @@
-import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
+import { onClick,getValue,setValue,hide,show } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.6/croot.js";
+import {postJSON,getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {addCSSIn} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
@@ -7,6 +8,20 @@ import { id,backend } from "/dashboard/jscroot/url/config.js";
 export async function main(){
     await addCSSIn("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css",id.content);
     getJSON(backend.project.data,'login',getCookie('login'),getResponseFunction);
+    onClick("tombolaksesmember",actionfunctionname);
+}
+
+function actionfunctionname(){
+    let idprjusr={
+        _id:getValue("project-name"),
+        phonenumber:getValue("phonenumber")
+    };
+    if (getCookie("login")===""){
+        redirect("/signin");
+    }else{
+        postJSON(backend.project.data,"login",getCookie("login"),idprjusr,postResponseFunction);
+        hide("tombolbuatproyek");
+    }  
 }
 
 function getResponseFunction(result){
@@ -26,4 +41,28 @@ function getResponseFunction(result){
             text: result.data.response
           });
     }
+}
+
+
+function postResponseFunction(result){
+    if(result.status === 200){
+        const katakata = "Berhasil memasukkan member baru ke project "+result.data.name;
+        Swal.fire({
+            icon: "success",
+            title: "Berhasil",
+            text: "Selamat kak proyek "+result.data.name+" dengan ID: "+result.data._id+" sudah mendapat member baru",
+            footer: '<a href="https://wa.me/62895601060000?text='+katakata+'" target="_blank">Verifikasi Proyek</a>',
+            didClose: () => {
+                setValue("phonenumber","");
+            }
+          });
+    }else{
+        Swal.fire({
+            icon: "error",
+            title: result.data.status,
+            text: result.data.response
+          });
+          show("tombolbuatproyek");
+    }
+    console.log(result);
 }
