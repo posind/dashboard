3f44a1721ddfb0da9b718667d37b7a646355c4af
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-25 15:22:39 +0700

    coba chat

--- jscroot/view/content/messages.js
+++ jscroot/view/content/messages.js
@@ -1,15 +1,50 @@
-import {addCSSIn} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
-import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
-import {id} from "../../url/config.js";
-
-
 export async function main(){
-    await addCSSIn("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css",id.content);
-    await addCSSIn("assets/css/forum.css",id.content);
-    Swal.fire({
-        icon: "success",
-        title: "Anda tidak memiliki pesan baru",
-        showConfirmButton: false,
-        timer: 1500
-    });
+    window.onload = chat;
 }
+
+function chat() {
+    var conn;
+    var msg = document.getElementById("msg");
+    var log = document.getElementById("log");
+  
+    function appendLog(item) {
+      var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
+      log.appendChild(item);
+      if (doScroll) {
+        log.scrollTop = log.scrollHeight - log.clientHeight;
+      }
+    }
+  
+    document.getElementById("form").onsubmit = function () {
+      if (!conn) {
+        return false;
+      }
+      if (!msg.value) {
+        return false;
+      }
+      conn.send(msg.value);
+      msg.value = "";
+      return false;
+    };
+  
+    if (window["WebSocket"]) {
+      conn = new WebSocket("wss://wss.do.my.id/ws");
+      conn.onclose = function (evt) {
+        var item = document.createElement("div");
+        item.innerHTML = "<b>Connection closed.</b>";
+        appendLog(item);
+      };
+      conn.onmessage = function (evt) {
+        var messages = evt.data.split('\n');
+        for (var i = 0; i < messages.length; i++) {
+          var item = document.createElement("div");
+          item.innerText = messages[i];
+          appendLog(item);
+        }
+      };
+    } else {
+      var item = document.createElement("div");
+      item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
+      appendLog(item);
+    }
+  }
