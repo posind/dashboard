769cf7eb54181bd10150ca0417f77dd033dcd10b
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-25 09:16:02 +0700

    backend

--- jscroot/view/content/home.js
+++ jscroot/view/content/home.js
@@ -1,15 +1,12 @@
 import {addCSSIn,setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
-import {id} from "../../url/config.js";
-
+import {id,backend} from "../../url/config.js";
 
 export async function main(){
     await addCSSIn("assets/css/admin.css",id.content);
     getJSON(backend.user.data,"login",getCookie("login"),getUserFunction);
     
 }
-
-
 
 function getUserFunction(result){
     if (result.status!==404){
