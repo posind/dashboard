56ec4f4a7f05299fa0ccd49074374bcd0bc3c174
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-26 21:10:43 +0700

    oke

--- jscroot/view/content/notifications/program.js
+++ jscroot/view/content/notifications/program.js
@@ -1,5 +1,53 @@
-import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.2/croot.js";
+import { onClick,getValue,disableInput,hide,show,onInput } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.7/croot.js";
+import {validatePhoneNumber} from "https://cdn.jsdelivr.net/gh/jscroot/validate@0.0.1/croot.js";
+import {postJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
+import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
+import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
+import {addCSSIn} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
+import { id, backend } from "/dashboard/jscroot/url/config.js";
 
-export function main(){
-    setInner("biggreet","Halo Ngadimin");
+export async function main(){
+    await addCSSIn("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css",id.content);
+    onInput('wa', validatePhoneNumber);
+    onClick("tombolpublishtask",actionfunctionname);
+}
+
+function actionfunctionname(){
+    let publish={
+        kode:getValue("kode"),
+        user:getValue("user"),
+        wa:getValue("wa"),
+        description:getValue("description")
+    };
+    if (getCookie("login")===""){
+        redirect("/signin");
+    }else{
+        postJSON(backend.project.data,"login",getCookie("login"),publish,responseFunction);
+        hide("tombolbuatproyek");
+    }  
+}
+
+function responseFunction(result){
+    if(result.status === 200){
+        const katakata = "Publish task baru "+result.data._id;
+        Swal.fire({
+            icon: "success",
+            title: "Berhasil",
+            text: "Selamat kak proyek "+result.data.name+" sudah terdaftar dengan ID: "+result.data._id+" dan Secret: "+result.data.secret,
+            footer: '<a href="https://wa.me/62895601060000?text='+katakata+'" target="_blank">Verifikasi Proyek</a>',
+            didClose: () => {
+                disableInput("name");
+                disableInput("description");
+            }
+          });
+    }else{
+        Swal.fire({
+            icon: "error",
+            title: result.data.status,
+            text: result.data.response
+          });
+          show("tombolbuatproyek");
+    }
+    console.log(result);
 }
