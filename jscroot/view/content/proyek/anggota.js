ccb3f8445964f24b463a8a32fd4b9191b34a2a71
daulJsx <mrdaul09@gmail.com>
2024-05-31 23:40:42 +0700

    deleted: delete button on notification

--- jscroot/view/content/proyek/anggota.js
+++ jscroot/view/content/proyek/anggota.js
@@ -20,17 +20,6 @@ export async function main() {
     getCookie("login"),
     getResponseFunction
   );
-
-  document.addEventListener("DOMContentLoaded", () => {
-    (document.querySelectorAll(".notification .delete") || []).forEach(
-      ($delete) => {
-        const $notification = $delete.parentNode;
-        $delete.addEventListener("click", () => {
-          $notification.parentNode.removeChild($notification);
-        });
-      }
-    );
-  });
 }
 
 function getResponseFunction(result) {
