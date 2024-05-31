51af8020092b5c8bb62a93133f636354eb8fad95
daulJsx <mrdaul09@gmail.com>
2024-05-31 22:19:53 +0700

    added: datatables on member page

--- jscroot/view/content/proyek/anggota.js
+++ jscroot/view/content/proyek/anggota.js
@@ -3,6 +3,7 @@ import { getCookie } from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croo
 import { addCSSIn } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js";
 import { id, backend } from "/dashboard/jscroot/url/config.js";
+import DataTable from "datatables.net-dt";
 
 export async function main() {
   await addCSSIn(
@@ -15,6 +16,10 @@ export async function main() {
     getCookie("login"),
     getResponseFunction
   );
+
+  await $(document).ready(function () {
+    $("#myTable").DataTable();
+  });
 }
 
 function getResponseFunction(result) {
