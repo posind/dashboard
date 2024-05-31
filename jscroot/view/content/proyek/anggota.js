20d26933d2c0f061cb335b27a6bac4b63f019ef8
daulJsx <mrdaul09@gmail.com>
2024-05-31 23:30:30 +0700

    fixed: importing loadScript function

--- jscroot/view/content/proyek/anggota.js
+++ jscroot/view/content/proyek/anggota.js
@@ -3,7 +3,7 @@ import { getCookie } from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croo
 import { addCSSIn } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js";
 import { id, backend } from "/dashboard/jscroot/url/config.js";
-import { loadScript } from "../../../controller/main";
+import { loadScript } from "../../../controller/main.js";
 
 export async function main() {
   await addCSSIn(
