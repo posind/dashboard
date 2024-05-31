9d3a774aaa100eda8a6e7c405a0caf29e4b0b1bf
daulJsx <mrdaul09@gmail.com>
2024-05-31 22:47:52 +0700

    fixed: datatables on member page[2]

--- jscroot/view/content/proyek/anggota.js
+++ jscroot/view/content/proyek/anggota.js
@@ -3,7 +3,6 @@ import { getCookie } from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croo
 import { addCSSIn } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js";
 import { id, backend } from "/dashboard/jscroot/url/config.js";
-import DataTable from "https://cdn.datatables.net/2.0.8/js/dataTables.js";
 
 export async function main() {
   await addCSSIn(
