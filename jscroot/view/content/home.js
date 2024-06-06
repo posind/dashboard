4c29a7b2177ad863b782f866dee1f521ab72e0a4
daulJsx <mrdaul09@gmail.com>
2024-06-06 15:34:53 +0700

    adj: main branch adjustments

--- jscroot/view/content/home.js
+++ jscroot/view/content/home.js
@@ -1,18 +1,24 @@
-import {addCSSIn,setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
-import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
-import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
-import {id,backend} from "../../url/config.js";
+import {
+  addCSSIn,
+  setInner,
+} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import { getCookie } from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
+import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
+import { id, backend } from "../../url/config.js";
 
-export async function main(){
-    await addCSSIn("assets/css/admin.css",id.content);
-    getJSON(backend.user.data,"login",getCookie("login"),getUserFunction);
-    
+export async function main() {
+  await addCSSIn("assets/css/admin.css", id.content);
+  getJSON(backend.user.data, "login", getCookie("login"), getUserFunction);
 }
 
-function getUserFunction(result){
-    if (result.status!==404){
-        setInner("biggreet","Halo "+result.data.name);
-    }else{
-        redirect("/signup");
-    }
+function getUserFunction(result) {
+  if (result.status !== 404) {
+    setInner("biggreet", "Halo " + result.data.name);
+    setInner(
+      "subtitle",
+      "Jumlah poin kamu saat ini sebesar " + result.data.poin + " poin."
+    );
+  } else {
+    redirect("/signup");
+  }
 }
