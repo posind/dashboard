969354824d88c90f5a7fcef10b4411606a680372
daulJsx <mrdaul09@gmail.com>
2024-06-13 09:52:41 +0700

    fixed: displaying rounded point

--- jscroot/view/content/home.js
+++ jscroot/view/content/home.js
@@ -13,10 +13,11 @@ export async function main() {
 
 function getUserFunction(result) {
   if (result.status !== 404) {
+    const roundedPoin = Math.round(result.data.poin);
     setInner("biggreet", "Halo " + result.data.name);
     setInner(
       "subtitle",
-      "Jumlah poin kamu saat ini sebesar " + result.data.poin + " poin."
+      "Jumlah poin kamu saat ini sebesar " + roundedPoin + " poin."
     );
   } else {
     redirect("/signup");
