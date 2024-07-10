527e81db565237f0427636216ba50378a89faf4b
daulJsx <mrdaul09@gmail.com>
2024-07-10 13:42:47 +0700

    Feat: Implement logout logic with js-cookie and SweetAlert

--- jscroot/view/navbar.js
+++ jscroot/view/navbar.js
@@ -32,3 +32,25 @@ function activeLink(className){//'.nav__link'
     } );});
 }
 
+/*==================== LOGOUT LOGIC ====================*/
+const logoutButton = document.querySelector(".nav__logout");
+
+if (logoutButton) {
+  logoutButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    Cookies.remove("login", {
+      path: "/",
+    });
+    Swal.fire({
+      icon: "success",
+      title: "Logged Out",
+      text: "You have been successfully logged out",
+      showConfirmButton: false,
+      timer: 2000,
+    }).then(() => {
+      window.location.href = "www.do.my.id";
+    });
+  });
+}
+
+
