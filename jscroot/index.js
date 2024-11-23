679e6fcef6049daf9e56d269218c64ab0a7fa007
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-11-23 20:05:27 +0700

    off dulu login

--- jscroot/index.js
+++ jscroot/index.js
@@ -10,10 +10,10 @@ import { getContentURL } from "./url/content.js";
 import {runAfterHeader,runAfterContent,runAfterHashChange} from "./controller/main.js";
 
 
-//check cookie login
-if (getCookie("login")===""){
-    redirect("/signin");
-}
+// //check cookie login
+// if (getCookie("login")===""){
+//     redirect("/signin");
+// }
 
 //adding CSS
 addCSS("https://unpkg.com/bulma@0.9.4/css/bulma.min.css");
